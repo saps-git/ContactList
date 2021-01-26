1d3207eae2b62ca1c0b3fa2f1b20098b0e843864
Saptarshi Hore <saptarshi0009@gmail.com>
2021-01-26 18:27:25 +0530

    cors whitelist regex

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -145,6 +145,11 @@ CORS_ALLOWED_ORIGINS = [
     "https://sub.example.com",
     "http://localhost:3000",
     "http://127.0.0.1:9000"
+    "http://127.0.0.1:8080"
+]
+
+CORS_ORIGIN_REGEX_WHITELIST = [
+    r"^https://\w+\.netlify\.app$",
 ]
 
 
