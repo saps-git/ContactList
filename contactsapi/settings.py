73800c148cd59e075410704b0a5561f38eb1098f
Saptarshi Hore <saptarshi0009@gmail.com>
2021-01-26 18:58:08 +0530

    cors_update

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -144,7 +144,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:3000",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
     "http://127.0.0.1:8080"
 ]
 
